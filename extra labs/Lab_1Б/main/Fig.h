a174b2913696d80a7ad25c242e0b314d53eaceaf
Dmitry <dima.shevchenko10@gmail.com>
2021-09-08 12:00:50 +0300

    12

---
+++ extra labs/Lab_1Б/main/Fig.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <windows.h>
+
+using namespace std;
+
+class Figure {
+	int p;
+	double S;
+public:
+	void countReg(int a, int b, int c);
+	void countSq(int r);
+};
---
+++ extra labs/Lab_1Б/main/main.cpp
@@ -0,0 +1,7 @@
+﻿#include "Fig.h"
+
+int main() {
+
+
+	return 0;
+}
