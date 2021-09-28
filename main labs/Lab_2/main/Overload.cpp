aa9e5c269167c93b86ee95f4908ec6528a3509b9
Dmitry <dima.shevchenko10@gmail.com>
2021-09-28 19:43:26 +0300

    with out [] ()

--- main labs/Lab_2/main/Overload.cpp
+++ main labs/Lab_2/main/Overload.cpp
@@ -1,34 +1,42 @@
 #include "Overload.h"
 
 overload::overload() {
-	name = "---";
-	type = '-';
+	type = '_';
 	temperature = 0;
 	speed = 0;
 }
 
-overload::overload(string &name, char& type, int& temperature, unsigned int& speed){
-	this->name = name;
+overload::overload(const char* name, string type, int temperature, unsigned int speed){
+	this->name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
+
 	this->type = type;
 	this->temperature = temperature;
 	this->speed = speed;
 }
 
-void overload::set(const string n, const char t, const int tem, const unsigned int sp) {
-	this->name = n;
+void overload::set(char* n, const string t, const int tem, const unsigned int sp) {
+	strcpy(this->name, n);
 	this->type = t;
 	this->temperature = tem;
 	this->speed = sp;
 
 }
 
-void overload::get(string& a, char& b, int& c, unsigned int& d)const {
+void overload::get(char* a, string& b, int& c, unsigned int& d)const {
+	delete[] a;
+	a = new char[strlen(this->name) + 1];
+	strcpy(a, this->name);
 
-	a = name;
 	b = type;
 	c = temperature;
 	d = speed;
 
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << d << " ";
+	cout << "\n";
 }
 
 void overload::show()const {
@@ -38,36 +46,95 @@ void overload::show()const {
 	cout << speed << " ";
 }
 
-bool  overload::operator == (const overload& other) {
+void  overload::operator == (const overload& other) {
 
-	if (this->name != other.name)
-		return 0;
+	if (strcmp(this->name, other.name))
+		cout << "Данные экземпляры класса не равны." <<endl;
 	else if (this->type != other.type)
-		return 0;
+		cout << "Данные экземпляры класса не равны." << endl;
 	else if (this->temperature != other.temperature)
-		return 0;
+		cout << "Данные экземпляры класса не равны." << endl;
 	else if (this->speed != other.speed)
-		return 0;
-	else return 1;
+		cout << "Данные экземпляры класса не равны." << endl;
+	else cout<< "Экземпляры класса равны."<<endl;
 
 }
 
 overload overload::operator = (const overload& other) {
-
-	this->name = other.name;
+	delete[] this->name;
+	this->name = new char[strlen(other.name) + 2];
+	if (!this->name) {
+		cout << "Ошибка! Память не выделена.";
+		exit(1);
+	}
+	strcpy(name, other.name);
 	this->type = other.type;
 	this->temperature = other.temperature;
 	this->speed = other.speed;
-	return other;// a sho esli ne vozvrashat`
-
+	return other;
 }
 
 overload overload::operator + (const overload& other) {
 
 	overload temp;
-	temp.name = this->name + other.name;
-
-	temp.type = this->temperature + other.temperature;
+	delete[] temp.name;
+	temp.name = new char(strlen(name) + strlen(other.name) + 2);
+	strcpy(temp.name, name);
+	strcat(temp.name, other.name);
+	temp.type = this->type + other.type;
+	temp.temperature = this->temperature + other.temperature;
 	temp.speed = this->speed + other.speed;
 	return temp;
+}
+
+int isvalid(int a, int b) {
+	
+	if (((a > N - 1) || (a < 0)) || ((b > N - 1) || (b < 0)))
+	{
+		cout << "Ошибка! Экземпляра с таким индексом не существует.\n";
+		_getch();
+		return -1;
+	}
+	else if (a == b)
+	{
+		cout << "Ошибка! Экземпляр не может быть записан сам в себя.\n";
+		_getch();
+		return -1;
+	}
+	return 0;
+}
+
+ostream& operator<<(ostream& stream, overload& other) {
+	shapka();
+	stream << "|" << setw(11) << other.name << "|";
+	stream << setw(7) << other.type << "|";
+	stream << setw(11) << other.temperature << "|";
+	stream << setw(13) << other.speed << "|";
+	linebuild();
+	return stream;
+}
+
+istream& operator>>(istream& stream, overload& other) {
+	cout << "Название, Школа, Количество монахов, Площадь земли: \n";
+	stream >> other.name;
+	stream >> other.type;
+	stream >> other.temperature;
+	stream >> other.speed;
+	return stream;
+}
+
+void shapka(void)
+{
+	
+	cout << "_______________________________________________\n";
+	cout << "|          Скорость звука в жидкостях         |\n";
+	cout << "|---------------------------------------------|\n";
+	cout << "| Вещество  |  Тип  |Температура|  Скорость   |\n";
+	cout << "|           |       |  (град.С) |   (м/сек)   |\n";
+	cout << "|-----------|-------|-----------|-------------|\n";
+}
+
+void linebuild(void) {
+
+	cout << "\n|---------------------------------------------|\n";
 }
--- main labs/Lab_2/main/Overload.h
+++ main labs/Lab_2/main/Overload.h
@@ -1,27 +1,37 @@
 #pragma once
+#define  _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "windows.h"
 #include <string.h>
+#include <iomanip>
+#include <conio.h>
 
 #define N 3
 
 using namespace std;
 
 class overload {
-	string name;
-	char type;
+	char* name;
+	string type;
 	int temperature;
 	unsigned int speed;
+	friend void shapka();
+	friend void linebuild();
+	friend int isvalid(int a, int b);
+	friend istream& operator>>(istream& stream, overload& other);
+	friend ostream& operator<<(ostream& stream, overload & o1);
 
 public:
 	overload();
-	overload(string &name, char &type, int &temperature, unsigned int &speed);
-	void set(const string n, const char t, const int tem, const unsigned int sp);
-	void get(string& a, char& b, int& c, unsigned int& d)const;
+	overload(const char* name, string type, int temperature, unsigned int speed);
+	void set(char* n, const string t, const int tem, const unsigned int sp);
+	void get(char* a, string& b, int& c, unsigned int& d)const;
 	void show()const;
-	bool operator == (const overload &other);
+	void operator == (const overload &other);
 	overload operator + (const overload& other);
 	overload operator = (const overload& other);
-
-
 };
+
+void shapka();
+void linebuild();
+int isvalid(int a, int b);
