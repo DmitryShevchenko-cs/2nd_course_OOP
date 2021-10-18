3626479c895f20bce184ff1501e3b99fb32b0ac9
Dmitry <dima.shevchenko10@gmail.com>
2021-10-18 16:33:01 +0300

    end

--- main labs/Lab_3/main/inheritance.h
+++ main labs/Lab_3/main/inheritance.h
@@ -9,40 +9,40 @@ class B1 {
 	int a;
 public:
 	B1(int x) { a = x; }
-	void show_B1() { cout << "B1 =   " << a << "\n";}
+	virtual void show() { cout << "B1 =   " << a << "\n";}
 };
 
 class B2 {
 	int b;
 public:
-	B2(int x) { b = x; }
-	void show_B2() { cout << "B2 =   " << b << "\n";}
+	B2(int x) { b = x; };
+	virtual void show() { cout << "B2 =   " << b << "\n";}
 };
 
 class D1 : protected B1 {
 	int c;
 public:
 	D1(int x, int y): B1(y) { c = x; }
-	void show_D1() { cout << "D1 =   " << c << "\n"; show_B1();}
+	void show() { cout << "D1 =   " << c << "\n"; B1::show();}
 };
 
 class D2: public B1 {
 	int d;
 public:
 	D2(int x, int y): B1(y) { d = x; }
-	void show_D2() { cout << "D2 =   " << d << "\n"; show_B1();}
+	void show() { cout << "D2 =   " << d << "\n"; B1::show();}
 };
 
 class D3 : protected B2, protected D2 {
-	int e;
+	int e;	
 public:
 	D3(int x, int y, int z, int i) : B2(y), D2(z, i) { e = x; }
-	void show_D3() { cout << "D3 =   " << e << "\n"; show_B2(); show_D2();}
+	void show() { cout << "D3 =   " << e << "\n"; B2::show(); D2::show();}
 };
 
 class D4: public D1, public D3 {
 	int f;
 public:
 	D4(int x, int y, int z, int i, int j, int g, int k): D1(y, z), D3( i, j, g, k) { f = x; }
-	void show_D4() { cout << "D4 =   " << f << "\n"; show_D1(); show_D3();}
+	void show_D4() { cout << "D4 =   " << f << "\n"; D1::show(); D3::show();}
 };
