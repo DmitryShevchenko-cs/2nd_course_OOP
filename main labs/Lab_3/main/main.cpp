5ef37ec5693dd18596e2fa8954f3ab30b99e8533
Dmitry <dima.shevchenko10@gmail.com>
2021-11-09 18:58:48 +0200

    finish

--- main labs/Lab_3/main/main.cpp
+++ main labs/Lab_3/main/main.cpp
@@ -5,7 +5,7 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	D4 temp(4, 9, -1, 6, 8, 2);
+	D4 temp(1, 2, 3, 4, 5, 6);
 	cout << "D4 temp(4, 9, -1, 6, 8, 2);\n";
 	cout << "\nСледуя иерархии класса D4: \n";
 	temp.show();
