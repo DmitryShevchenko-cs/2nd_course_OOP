b455cd6678ae7839cef8b3040373a82c1af73cc6
Dmitry <dima.shevchenko10@gmail.com>
2021-11-17 06:08:22 +0200

    finish

--- extra labs/Lab_7Б/Lab_7Б/Output.cpp
+++ extra labs/Lab_7Б/Lab_7Б/Output.cpp
@@ -3,15 +3,13 @@
 
 ostream& operator << (ostream& s, const OUTPUT& ob) {
 
-	
-		for (int i = 0; i < ob.str.length(); i++) {
+	for (int i = 0; i < ob.str.length(); i++) {
 
-			if ((ob.str[i] < 48 || ob.str[i] > 58) && (ob.str[i] < 'À' || ob.str[i] > 'ß') && (ob.str[i] < 'A' || ob.str[i] > 'Z'))
-				s << ob.str[i];
-			else s << ' ';
+		if ((ob.str[i] < 48 || ob.str[i] > 58) && (ob.str[i] < 'À' || ob.str[i] > 'ß') && (ob.str[i] < 'A' || ob.str[i] > 'Z'))
+			s << ob.str[i];
+		else s << ' ';
 
-		}
-	
+	}
 
 	return s;
 }
--- extra labs/Lab_7Б/Lab_7Б/main.cpp
+++ extra labs/Lab_7Б/Lab_7Б/main.cpp
@@ -6,16 +6,11 @@ int main() {
 	SetConsoleOutputCP(1251);
 
 	OUTPUT ob("123ЫЫЫтест11SDHтекст");
-	
+
 	ob.show();
 	cout << ob;
-
-	fstream fil("text.txt", ios::in | ios::app);
-	
-	if (fil.is_open())
-		fil << ob << endl;
-
-	else cout << "error";
+	ofstream fil("text.txt", ios::in | ios::app);
+	fil << ob << endl;
 
 	fil.close();
 }
